eb8c46af104ecc86029a2fdd031f8810000e65b8
Yaroslav <zinakola2004@gmail.com>
2023-09-03 01:20:30 +0500

    Add data layer in project

---
+++ app/src/main/java/com/example/letsgocount/data/GameRepositoryImpl.kt
@@ -0,0 +1,68 @@
+package com.example.letsgocount.data
+
+import com.example.letsgocount.domain.entities.GameSettings
+import com.example.letsgocount.domain.entities.Level
+import com.example.letsgocount.domain.entities.Question
+import com.example.letsgocount.domain.repository.GameRepository
+import kotlin.random.Random
+
+object GameRepositoryImpl : GameRepository {
+
+    private const val MIN_SUM_FOR_QUESTION = 2
+    private const val MIN_VISIBLE_NUMBER_VALUE = 1
+
+    override fun generateNewQuestion(maxSum: Int, countOfAnswers: Int): Question {
+        val seedForRandom = System.currentTimeMillis()
+        val sumNumber = Random(seedForRandom).nextInt(MIN_SUM_FOR_QUESTION,maxSum + 1)
+        val visibleNumber = Random.nextInt(MIN_VISIBLE_NUMBER_VALUE, sumNumber)
+        //использует HashSet для того чтобы исключить возможность содержания одинаковых чисел
+        val listOfAnswers = HashSet<Int>()
+        val rightAnswer = sumNumber - visibleNumber
+        listOfAnswers.add(rightAnswer)
+        while (listOfAnswers.size < countOfAnswers) {
+            val oneOfAnswer = (MIN_VISIBLE_NUMBER_VALUE..maxSum).random()
+            listOfAnswers.add(oneOfAnswer)
+        }
+
+        return Question(sumNumber, visibleNumber, listOfAnswers.toList())
+    }
+
+    override fun getGameSettings(level: Level): GameSettings {
+        when (level) {
+            Level.TEST -> {
+                return GameSettings(
+                    10,
+                    3,
+                    30,
+                    10
+                )
+            }
+            Level.EASY -> {
+                return GameSettings(
+                    10,
+                    5,
+                    50,
+                    40
+                )
+            }
+            Level.NORMAL -> {
+                return GameSettings(
+                    30,
+                    10,
+                    70,
+                    30
+                )
+            }
+            Level.HARD -> {
+                return GameSettings(
+                    50,
+                    15,
+                    90,
+                    20
+                )
+            }
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/letsgocount/presentation/MainActivity.kt
+++ app/src/main/java/com/example/letsgocount/presentation/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.letsgocount.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.letsgocount.R
+import com.example.letsgocount.data.GameRepositoryImpl
+import com.example.letsgocount.domain.usecases.UseCaseGenerateQuestion
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +14,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+
+    }
+
+    companion object {
+        private const val TAG = "MAIN_ACTIVITY"
     }
 }
